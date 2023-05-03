ef873db9243a8f447eda1c663e1610c8ceae536f
Noor Alam <alamrangrej787@gmail.com>
2023-05-03 18:08:59 +0530

    JS Revision

---
+++ JSPractice/p.js
@@ -0,0 +1,60 @@
+//callback
+// setTimeout(()=>{
+//     console.log("Noor Alam")
+// }, 1000);
+
+//
+// const logHello=()=>{
+//     console.log("Noor Alam")
+// }
+// setTimeout(logHello,1000)
+
+//
+// function a() {
+//     setTimeout(() => {
+//         console.log("a");
+//     }, 2000)
+// }
+
+// function b(callback) {
+//     setTimeout(() => {
+//         console.log("b");
+//         callback();
+//     }, 1000)
+// }
+
+// b(a);
+
+// var a = 10;
+// function x() {
+//     console.log(a);
+//     var a = 20;
+// }
+
+// x();
+
+
+// const a = 11;
+// const result = (a%2==0)?"even":"odd";
+// console.log(result)
+
+
+// setInterval(()=>{
+//     console.log("Noor Alam")
+// },1000)
+
+
+
+// function a(noor){
+//     setTimeout(()=>{
+//         console.log("alam")
+//     })
+// }
+// a(alam);
+
+
+//IIFE
+var numbers = [1, 2, 3, 4, 5, ,6 ,7, 8]
+const value = numbers.filter<5;
+console.log(value);
+
